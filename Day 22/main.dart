227602579d6955f819f78fd72acd7609505c740f
Jayaprakash B <161607708+JayaprakashB27@users.noreply.github.com>
2025-06-09 18:38:53 +0530

    Add files via upload

--- Day 22/main.dart
+++ Day 22/main.dart
@@ -2,64 +2,161 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(TextfieldEx());
+  runApp(NavigatorEx2());
 }
 
-class TextfieldEx extends StatelessWidget {
-  const TextfieldEx({super.key});
+class NavigatorEx2 extends StatelessWidget {
+  const NavigatorEx2({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: CupertinoColors.white,
         appBar: AppBar(
-          title: Text('Text Field Example'),
           centerTitle: true,
-          toolbarHeight: 60,
-          backgroundColor: Colors.lightGreen,
-        ),
-        body: Padding(
-          padding: EdgeInsetsGeometry.all(20),
-          child: Column(
+          backgroundColor: CupertinoColors.activeOrange,
+          title: ListTile(
+              // leading: Icon(Icons.food_bank),
+             contentPadding: EdgeInsetsGeometry.only(top: 60,bottom: 60,left: 500),
+              title:Text(' Bank Of India'),
+              subtitle: Text('Save your money with us'),
+            ),
+          ),
+        //  backgroundColor: Colors.orangeAccent,
+        // drawer: Drawer(
+        //   backgroundColor: CupertinoColors.systemYellow,
+        //   child: Column(
+        //     children: [
+        //       Text('Contact',textAlign: TextAlign.left,),
+        //       Text('Chat'),
+        //       Text('Settings'),
+        //       Text('more..')
+        //     ],
+        //   ),
+        // ),
+
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
             children: [
-              TextField(
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 25,
-                  fontFamily: 'sansserif',
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color(0xFF062481),
                 ),
-                keyboardType: TextInputType.text,
-                obscureText: false,
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.person),
-                    labelText: "Username",
-                    hintText: "jayaprakash ",
-           //         border: UnderlineInputBorder(),
-                  border: UnderlineInputBorder(borderRadius: BorderRadius.circular(100)),
-                  fillColor: Colors.blueAccent,
-                  filled: true,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB-LymyScqLuK8nsUfr-eBSXB8hS1dyh2rcTvYpSoS0saWVopeWU-ngeHghYyQaWLcQ8I&usqp=CAU'),
+                    ),
+                    SizedBox(height: 20),
+                    Text('Hi, Jayaprakash',
+                        style: TextStyle(color: Colors.white, fontSize: 18)),
+                    Text('Customer ID: XXXXXX436',
+                        style: TextStyle(color: Colors.white70, fontSize: 12)),
+                  ],
                 ),
               ),
-              SizedBox(height: 30,),
-              TextField(
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 25,
-                  fontFamily: 'sansserif',
-                ),
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.password_rounded),
-                  labelText: "Password :",
-                  hintText: "12345678",
-                  border: OutlineInputBorder(),
-                ),
-              )
+              ListTile(
+                leading: Icon(Icons.dashboard),
+                title: Text('Dashboard'),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_balance_wallet),
+                title: Text('Accounts'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.swap_horiz),
+                title: Text('Transactions'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.support_agent),
+                title: Text('Support'),
+                onTap: () {},
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.logout, color: Colors.red),
+                title: Text('Logout', style: TextStyle(color: Colors.red)),
+                onTap: () {
+                  // print('You are logged out successfully!);
+                },
+              ),
             ],
           ),
-          ),
         ),
-      );
+
+    body: Padding(
+      padding: EdgeInsets.all(70),
+        child: Row(
+          children: [
+            Expanded(
+                flex: 3,
+                 child: Column(
+                   children: [
+                     Expanded(
+                       child: Row(
+                         children: [
+                           Expanded(
+                             child: Card(
+                               color: CupertinoColors.activeOrange,
+                               child: Center(child: Text('Transactions')),
+                             ),
+                           ),
+                           SizedBox(width: 60),
+                           Expanded(
+                             child: Card(
+                               color: CupertinoColors.activeOrange,
+                               child: Center(child: Text('History')),
+                             ),
+                           ),
+                         ],
+                       ),
+                     ),
+                     SizedBox(height: 50),
+                     Expanded(
+                       child: Row(
+                         children: [
+                           Expanded(
+                             child: Card(
+                               color: CupertinoColors.activeOrange,
+                               child: Center(child: Text('Check balance')),
+                             ),
+                           ),
+                           SizedBox(width: 250),
+                           Expanded(
+                             child: Card(
+                               color: CupertinoColors.activeOrange,
+                               child: Center(child: Text('Bank Transfer')),
+                             ),
+                           ),
+                         ],
+                       ),
+                     ),
+                   ],
+                 ),
+            ),
+            SizedBox(width: 110),
+            Expanded(
+              flex: 1,
+              child: Card(
+                child: Container(
+                  height: double.infinity,
+                  color: CupertinoColors.activeOrange,
+                  child: Center(child: Text('Accounts & updates')),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      )
+    );
   }
 }
